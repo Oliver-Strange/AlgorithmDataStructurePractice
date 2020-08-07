dff58d4209fe8a9c17e74df59758858cc9e6ca6c
OliverStrange <preston.burton@gmail.com>
2020-08-07 14:11:26 -0700

    reverse string and palindrome solved

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  //   if (str.split("").reverse().join("") === str) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  //   const reversed = str.split('').reverse().join('')
+  //   return reversed === str
+  // return str.split("").every((char, index) => {
+  //   return char === str[str.length - index - 1];
+  // });
+}
 
 module.exports = palindrome;
